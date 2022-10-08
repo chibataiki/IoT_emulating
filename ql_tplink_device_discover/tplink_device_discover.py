c616957b5dd91d4e8f458d8eb867a43782c74788
chibataiki <polaalemu@gmail.com>
2022-10-07 17:30:39 -0700

    update

--- ql_tplink_device_discover/tplink_device_discover.py
+++ ql_tplink_device_discover/tplink_device_discover.py
@@ -30,20 +30,26 @@ def my_setsockopt(ql: Qiling):
 def my_sendto(ql: Qiling):
     return 0
 
+def my_ioctl(ql: Qiling):
+    print("[+]Hooked ioctl")
+    return 0
 
 def hook_to_packet_handle(ql: Qiling):
     ql.arch.regs.write("pc", 0x00403978)
 
+
+# useless if hijack ioctl
 def hook_004034D4(ql: Qiling):
     ql.arch.regs.write("v0", 0x1)
 
 def hook_common_timer(ql: Qiling):
     ql.arch.regs.write("v0",0x1)
+
 def hook_0x4056e0(ql: Qiling):
     # 00403220 common_timer
     ql.arch.regs.write("pc",0x00403220)
 def my_sandbox(path, rootfs):
-    ql = Qiling(path, rootfs, verbose=QL_VERBOSE.DEFAULT)
+    ql = Qiling(path, rootfs, verbose=QL_VERBOSE.DEBUG)
     ql.root = True
     ql.debugger = True
 
@@ -52,16 +58,17 @@ def my_sandbox(path, rootfs):
     init_sock_addr = 0x00402BA8
     after_initrsock_addr = 0x004038E8
     to_common_timer =  0x00403488
-    #hook_init_sock()
+
+    ql.patch(0x0040BAC0, b'lo\x00')
+
     # after finish sock init, jump to packet handle function
     ql.hook_address(hook_to_packet_handle,after_initrsock_addr)
-    ql.hook_address(hook_004034D4, 0x04034D4)
-    ql.patch(0x0040BAC0, b'lo\x00')
     ql.hook_address(hook_common_timer,to_common_timer)
-  #  ql.hook_address(hook_0x4056e0,0x4056e0)
-    ql.os.set_api('setsockopt', my_setsockopt, QL_INTERCEPT.CALL)
-    ql.os.set_api('sendto', my_sendto,QL_INTERCEPT.CALL)
+  # ql.hook_address(hook_0x4056e0,0x4056e0)
 
+    ql.os.set_api('setsockopt', my_setsockopt, QL_INTERCEPT.CALL)
+  # ql.os.set_api('sendto', my_sendto,QL_INTERCEPT.CALL)
+    ql.os.set_api('ioctl', my_ioctl,QL_INTERCEPT.CALL)
     ql.run()
 
 if __name__ == "__main__":
